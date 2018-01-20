fb5a255f5185b2cea76970eddf07e52774600996
Olga-491 <st040357@student.spbu.ru>
2018-01-20 15:21:26 +0300

    test

--- logout.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-def logout (a):
-	if len (a) > 1:
-		json_string=a[1]
-	else: json_string="###"
-	print (json_string)
-	return json_string
-logout(sys.argv)
--- reg.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-def reg (a):
-	if len (a) > 1:
-		json_string=a[1]
-	else: json_string="###"
-	print (json_string)
-	return json_string
-reg(sys.argv)
