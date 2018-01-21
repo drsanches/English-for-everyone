a9bc89698c865674fb7512840f2c8854d69ccaf3
Olga-491 <st040357@student.spbu.ru>
2018-01-21 22:22:35 +0300

    getlesson.py

--- SimpleLearnServer/src/cgi-bin/getlesson.py
+++ SimpleLearnServer/src/cgi-bin/getlesson.py
@@ -2,6 +2,7 @@ import cgi
 import json
 import sqlite3
 import db_address
+import random
 
 form = cgi.FieldStorage()
 
@@ -24,8 +25,37 @@ else:
             cursor.execute(query, (session_id,))
             if len(cursor.fetchall()) != 1:
                 raise Exception()
+            query = """SELECT NLang FROM Users left join Sessions ON Users.UserID = Sessions.UserID WHERE SessionID = ?"""
+            cursor.execute(query, (session_id,))
+            lang = cursor.fetchall()
+            query = """Select PairID, Words.LangID, Spell, Phonetic FROM WordsToLearn left join Pair ON WordsToLearn.PairID = Pair.ID
+                        left join Words On Pair.Word1ID = Words.WordID OR Pair.Word2ID = Words.WordID 
+                        left join Sessions ON Sessions.UserID = WordsToLearn.UserID
+                        WHERE SessionID = ?"""
+            cursor.execute(query, (session_id,))
+            rows = cursor.fetchall()
 
+            lesson = []
+            for i in range(5):
+                number = random.randint(1, len(rows))
+                pair = {}
+                if rows[number][1] == lang:
+                    pair["NativeWord Spell"] = rows[number][2]
+                    pair["NativeWord Phonetic"] = rows[number][3]
+                    for row in rows:
+                        if row[0] == number and row[1] != lang:
+                            pair["ForeignWord Spell"] = row[2]
+                            pair["ForeignWord Phonetic"] = row[3]
+                else:
+                    pair["ForeignWord Spell"] = rows[number][2]
+                    pair["ForeignWord Phonetic"] = rows[number][3]
+                    for row in rows:
+                        if row[0] == number and row[1] != lang:
+                            pair["ForeignWord Spell"] = row[2]
+                            pair["ForeignWord Phonetic"] = row[3]
 
+                lesson.append(pair)
+            response["Lesson"] = lesson
 
             connection.commit()
             connection.close()
@@ -33,23 +63,6 @@ else:
             response["Status"] = "Success"
         except:
             response["Status"] = "Failure"
-        lesson = []
-        for i in range(5):
-            pair = {}
-            pair["NativeWord"] = "Nativeword" + str(i)
-            pair["ForeignWord"] = "Foreignword" + str(i)
-            lesson.append(pair)
-        response["Lesson"] = lesson
-    else:
-        response["Status"] = "Success"
-        lesson = []
-        for i in range(5):
-            pair = {}
-            pair["NativeWord"] = "Nativeword" + str(i)
-            pair["ForeignWord"] = "Foreignword" + str(i)
-            lesson.append(pair)
-        response["Lesson"] = lesson
-
 
 print("Content-type: application/json")
 print()
