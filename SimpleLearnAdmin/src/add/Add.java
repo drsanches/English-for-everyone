4e1219690b45425303ce840e30a2fd111964af6b
drsanches <dr.sanches3@gmail.com>
2018-01-22 17:59:58 +0300

    Getstat on client was changed

--- SimpleLearnAdmin/src/add/Add.java
+++ SimpleLearnAdmin/src/add/Add.java
@@ -2,10 +2,7 @@ package add;
 
 import org.json.*;
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Add
@@ -29,18 +26,43 @@ public class Add
     private static void addDictionaryFile(String filePath, String dbPath) {
         try {
             String stringDictionary = "";
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF8"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF8"));
             String nextString;
-            while ((nextString = br.readLine()) != null) {
+            while ((nextString = reader.readLine()) != null) {
                 stringDictionary += nextString;
             }
             stringDictionary = stringDictionary.substring(1, stringDictionary.length());
+            reader.close();
 
             JSONObject jsonDictionary = new JSONObject(stringDictionary);
             String theme = jsonDictionary.getString("Theme");
             String level = jsonDictionary.getString("Level");
             String nativeLanguage = jsonDictionary.getString("NativeLanguage");
             String foreignLanguage = jsonDictionary.getString("ForeignLanguage");
+
+            Class.forName("org.sqlite.JDBC");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+            PreparedStatement pstmt;
+
+            String sql = "INSERT INTO Dictionary(LevelID, Theme)\n" +
+                    "SELECT LevelID, ?\n" +
+                    "FROM Level\n" +
+                    "WHERE LevelName = ?;";
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, theme);
+            pstmt.setString(2, level);
+            pstmt.executeUpdate();
+
+            sql = "SELECT DictionaryID FROM Dictionary " +
+                    "LEFT JOIN Level ON Dictionary.LevelID = Level.LevelID " +
+                    "WHERE Theme = ? AND LevelName = ?;";
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, theme);
+            pstmt.setString(2, level);
+            ResultSet resultSet = pstmt.executeQuery();
+            String dictionaryId = resultSet.getString(1);
+
+
             JSONArray words = (JSONArray) jsonDictionary.getJSONArray("Words");
 
             for (int i = 0; i < words.length(); i++) {
@@ -49,9 +71,50 @@ public class Add
                 String nativeTranscription = word.getString("NativeTranscription");
                 String foreignWord = word.getString("ForeignWord");
                 String foreignTranscription = word.getString("ForeignTranscription");
-            }
 
-            } catch ( Exception e ) {
+                sql = "INSERT INTO Words(LangID, Spell, Phonetic)\n" +
+                        "SELECT LangID, ?, ?\n" +
+                        "FROM Languages\n" +
+                        "WHERE LangName = ?;";
+
+                pstmt = connection.prepareStatement(sql);
+                pstmt.setString(1, nativeWord);
+                pstmt.setString(2, nativeTranscription);
+                pstmt.setString(3, nativeLanguage);
+                pstmt.executeUpdate();
+
+                pstmt.setString(1, foreignWord);
+                pstmt.setString(2, foreignTranscription);
+                pstmt.setString(3, foreignLanguage);
+                pstmt.executeUpdate();
+
+                sql = "SELECT WordID FROM Words WHERE Spell = ? AND Phonetic = ?;";
+
+                pstmt = connection.prepareStatement(sql);
+                pstmt.setString(1, nativeWord);
+                pstmt.setString(2, nativeTranscription);
+                resultSet = pstmt.executeQuery();
+                String nativeWordId = resultSet.getString(1);
+
+                pstmt = connection.prepareStatement(sql);
+                pstmt.setString(1, foreignWord);
+                pstmt.setString(2, foreignTranscription);
+                resultSet = null;
+                resultSet = pstmt.executeQuery();
+                String foreignWordId = resultSet.getString(1);
+
+                sql = "INSERT INTO Pair(DicID, Word1ID, Word2ID) VALUES(?, ?, ?)";
+
+                pstmt = connection.prepareStatement(sql);
+                pstmt.setString(1, dictionaryId);
+                pstmt.setString(2, nativeWordId);
+                pstmt.setString(3, foreignWordId);
+                pstmt.executeUpdate();
+            }
+            pstmt.close();
+            connection.close();
+            }
+            catch ( Exception e ) {
                 System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             }
     }
